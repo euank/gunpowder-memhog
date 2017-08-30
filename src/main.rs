bd3e569d0fc16388fe5577a626d8f32606b599fa
Euan Kemp <euan.kemp@coreos.com>
2017-08-30 13:30:27 -0700

    Update for modern rust
    This fixes warnings, as well as doing a rustfmt pass.
    
    The primary change is that sleep_ms is replaced with sleep

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ use argparse::{ArgumentParser, Store};
 use std::collections::HashMap;
 use std::vec::Vec;
 use std::str::FromStr;
+use std::thread::sleep;
 use std::thread;
+use std::time::Duration;
 use std::fmt;
 
 /// The `Bytes` type represents a number of bytes.
@@ -16,13 +18,13 @@ struct Bytes(i64);
 #[derive(Debug, Clone, PartialEq)]
 pub enum Error {
     UnrecognizedPrefix,
-    UnparseableInt
+    UnparseableInt,
 }
 impl Error {
     fn description(&self) -> &str {
         match *self {
             Error::UnrecognizedPrefix => "Unrecognized byte prefix",
-            Error::UnparseableInt => "Unparseable byte size"
+            Error::UnparseableInt => "Unparseable byte size",
         }
     }
 }
@@ -41,7 +43,7 @@ impl FromStr for Bytes {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut byte_size_map: HashMap<String, i64> = HashMap::new();
         // TODO, pow! macro
-        byte_size_map.insert( "b".to_string(), 1);
+        byte_size_map.insert("b".to_string(), 1);
         byte_size_map.insert("kb".to_string(), 1024);
         byte_size_map.insert("mb".to_string(), 1024 * 1024);
         byte_size_map.insert("gb".to_string(), 1024 * 1024 * 1024);
@@ -59,23 +61,27 @@ impl FromStr for Bytes {
         }
 
         // Normalize postfix string into lowercase with the 'b' postfix, so e.g. '100M' -> m
-        let mut postfix_str: String = chars.into_iter().map(|c| c.to_lowercase().next().unwrap()).collect();
-        if postfix_str.len() == 1  && postfix_str.chars().next().unwrap() != 'b' {
+        let mut postfix_str: String = chars
+            .into_iter()
+            .map(|c| c.to_lowercase().next().unwrap())
+            .collect();
+        if postfix_str.len() == 1 && postfix_str.chars().next().unwrap() != 'b' {
             postfix_str.push('b');
         }
         let mut multiplier: i64 = 1;
         if postfix_str.len() > 0 {
             match byte_size_map.get(&postfix_str) {
-                Some(num) => { multiplier = *num },
-                None => { return Err(Error::UnrecognizedPrefix) }
+                Some(num) => multiplier = *num,
+                None => return Err(Error::UnrecognizedPrefix),
             }
         }
 
         let tmpstr: String = nums.into_iter().collect();
-        let parsed: Result<i64, std::num::ParseIntError> = std::str::FromStr::from_str(tmpstr.as_ref());
+        let parsed: Result<i64, std::num::ParseIntError> =
+            std::str::FromStr::from_str(tmpstr.as_ref());
         match parsed {
             Ok(x) => Ok(Bytes(x * multiplier)),
-            _ => Err(Error::UnparseableInt)
+            _ => Err(Error::UnparseableInt),
         }
     }
 }
@@ -94,7 +100,7 @@ fn main() {
     let mut final_memory = Bytes(10 * 1024 * 1024);
     let mut start_memory = Bytes(0);
     let mut step_duration = 0f64;
-    let mut exit_timeout = 0f64;
+    let mut exit_timeout = 0u64;
     {
         let mut ap = ArgumentParser::new();
         ap.set_description("Take up memory");
@@ -102,16 +108,22 @@ fn main() {
             .add_argument("final memory", Store, "Memory to reach after timeout");
         ap.refer(&mut start_memory)
             .add_option(&["-m", "--start-memory"], Store, "Starting memory");
-        ap.refer(&mut exit_timeout)
-            .add_option(&["-e", "--exit-timeout"], Store, "How long to run until exiting in seconds (0 for forever)");
-        ap.refer(&mut step_duration)
-            .add_option(&["-t", "--step-timeout"], Store, "How long to take to increase memory for from initial -> final in seconds");
+        ap.refer(&mut exit_timeout).add_option(
+            &["-e", "--exit-timeout"],
+            Store,
+            "How long to run until exiting in seconds (0 for forever)",
+        );
+        ap.refer(&mut step_duration).add_option(
+            &["-t", "--step-timeout"],
+            Store,
+            "How long to take to increase memory for from initial -> final in seconds",
+        );
         ap.parse_args_or_exit();
     }
 
-    if exit_timeout > 0f64 {
+    if exit_timeout > 0 {
         thread::spawn(move || {
-            std::thread::sleep_ms((exit_timeout * 1000f64) as u32);
+            sleep(Duration::from_secs(exit_timeout));
             std::process::exit(0);
         });
     }
@@ -119,9 +131,11 @@ fn main() {
     waste_memory(start_memory.0 as usize);
 
     if step_duration > 0f64 {
-        for _ in 0..((step_duration*2f64) as i64) {
-            waste_memory(((final_memory.0 - start_memory.0) as f64 / (step_duration * 2f64)) as usize);
-            std::thread::sleep_ms(500);
+        for _ in 0..((step_duration * 2f64) as i64) {
+            waste_memory(
+                ((final_memory.0 - start_memory.0) as f64 / (step_duration * 2f64)) as usize,
+            );
+            sleep(Duration::from_millis(500));
         }
     } else {
         waste_memory((final_memory.0 - start_memory.0) as usize);
@@ -129,6 +143,6 @@ fn main() {
 
     println!("Done wasting memory");
     loop {
-        std::thread::sleep_ms(500);
+        sleep(Duration::from_millis(500));
     }
 }
